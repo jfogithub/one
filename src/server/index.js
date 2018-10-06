40fcf6170837ab8de7e08303604e5f2953482336
Johan Frennesson <jfo@SELUN-JFO-MAC.local>
2018-10-06 14:07:13 +0200

    one: image folder and virtual path

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+var path = require('path')
 
 const app = express()
 const port = 10000
+const basepath = '/'
+const packagefolder = 'src'
+const clientfolder = 'client'
+const imagefolder = 'images'
 
 const logInfo = (info) => console.log(info)
-const content = (req, res) => res.send('One')
 
-app.get('/', content)
+app.use(`${basepath}`, express.static(path.join(`${packagefolder}`, `${clientfolder}`)))
+app.use(`${basepath}images`, express.static(path.join(`${packagefolder}`, `${imagefolder}`)))
 
-app.listen(port, logInfo(`Listening on port ${port}!`))
+app.listen(port, logInfo(`Listening on port ${port}`))
 
 
